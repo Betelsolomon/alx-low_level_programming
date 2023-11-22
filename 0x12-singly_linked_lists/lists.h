f6a3719f2e7c0e4fc0410e1e93e7615246e36086
Betelsolomon <bebrave1214@gmail,com>
2023-11-21 23:00:10 -0800

    Updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,28 +1,36 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- * Description: singly linked list node structure
- *              for Holberton project
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 
-typedef struct list_s
+list_t *add_node(list_t **head, const char *str)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	char *dup;
+	int len;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+	list_t *new;
 
-#endif /* LISTS_H */
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
